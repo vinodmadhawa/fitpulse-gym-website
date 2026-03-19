74e13c122ecea2acbb965a5c88147d80f3c15b80
vinodmadhawa <priyadarshanamadhawa@gmail.com>
2026-03-19 22:31:49 +0530

    feat: add GitHub Actions workflow for deploying to GitHub Pages; include build and deployment steps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/fitpulse-gym-website/' : '/',
   plugins: [react()],
   server: {
     proxy: {
